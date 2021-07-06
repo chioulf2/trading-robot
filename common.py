4b5fea117acf7fd0c5a7e1ca57f4745554b7781d
liuqinh2s <liuqinh2s@gmail.com>
2021-07-06 19:24:58 +0800

    update: 调参

--- common.py
+++ common.py
@@ -18,7 +18,7 @@ def long(symbol, quantity, take_profit_scope, stop_scope):
         # 止盈
         take_profit_price = str(round(float(price) * (1 + take_profit_scope), 2))
         take_profit_orderId = order(symbol, 'SELL', 'LONG', 'TRAILING_STOP_MARKET', quantity, take_profit_price, '', '',
-                                    '0.5')
+                                    '1')
         # take_profit_orderId = order(symbol, 'SELL', 'LONG', 'LIMIT', quantity, take_profit_price)['orderId']
         globalVar['orderMap'][take_profit_orderId] = stop_orderId
         globalVar['orderMap'][stop_orderId] = take_profit_orderId
@@ -44,7 +44,7 @@ def short(symbol, quantity, take_profit_scope, stop_scope):
         # 止盈
         take_profit_price = str(round(float(price) * (1 - take_profit_scope), 2))
         take_profit_orderId = order(symbol, 'BUY', 'SHORT', 'TRAILING_STOP_MARKET', quantity, '', '', take_profit_price,
-                                    '0.5')
+                                    '1')
         # take_profit_orderId = order(symbol, 'BUY', 'SHORT', 'LIMIT', quantity, take_profit_price)['orderId']
         globalVar['orderMap'][take_profit_orderId] = stop_orderId
         globalVar['orderMap'][stop_orderId] = take_profit_orderId
