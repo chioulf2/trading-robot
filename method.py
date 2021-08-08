2fbde018ebd45b1d7fb341928e6ce06d3cb0f7f1
liuqinh2s <liuqinh2s@gmail.com>
2021-08-08 10:04:17 +0800

    新增停机字段

--- method.py
+++ method.py
@@ -3,6 +3,8 @@ from webSocketListener import WebSocketListener
 
 
 def addUser(strategy, u):
+    if u[7] == 'true':
+        return
     user = User(*u[:5])
     strategy.add(user)
     listener = WebSocketListener(user, None)
