2d052c9d8fdb7b5c0b069c52af7bf990a061b351
liuqinh2s <liuqinh2s@gmail.com>
2022-03-25 23:28:07 +0800

    打印信息调试

--- webSocketListener.py
+++ webSocketListener.py
@@ -23,6 +23,7 @@ class WebSocketListener(object):
         self.ws = None
 
     def handleClosePosition(self, message):
+        self.user.notifier.notify(json.dumps(message))
         if message['o']['x'] == "TRADE" and (
                 message['o']['X'] == "FILLED" or message['o']['X'] == "PARTIALLY_FILLED") and \
                 (message['o']['ot'] == "STOP_MARKET" or message['o']['ot'] == "TRAILING_STOP_MARKET" or
